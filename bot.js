935cc9afec22e2abec1bb07215f50c6db290aaa1
EmRowland <65784893+EmRowland@users.noreply.github.com>
2023-04-23 12:02:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 global.Api = new (require('./plugins/neoxrApi.js'))(process.env.API_KEY)
 
 //settings
-global.owner = ['+447441437150',''] //owner number ++447441437150
-global.wame = ['+447441437150'] 
-global.pemilik = ['+447441437150'] //owner number ++447441437150
-global.premium = ['+447441437150'] //premium number ++447441437150
+global.owner = ['+2348085275808',''] //owner number ++447441437150
+global.wame = ['+2348085275808'] 
+global.pemilik = ['+2348085275808'] //owner number ++447441437150
+global.premium = ['+2348085275808'] //premium number ++447441437150
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
